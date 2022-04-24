0fc409537e91b1f9f25814798cd9dfcc74b9492e
Kannan Chithambaranathan <ckannan759153@gmail.com>
2022-04-24 22:06:44 +0530

    Project Updated

--- EnterpriseApp/EnterpriseApp.Mobile/EnterpriseApp.Mobile/Views/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xamarin.Forms;
-
-namespace EnterpriseApp.Mobile.Views
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage() => InitializeComponent();
-    }
-}
